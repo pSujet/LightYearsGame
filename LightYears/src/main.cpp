b92dd19e5fdd553d3199b8734292bb1517684466
pSujet <sujetj@gmail.com>
2024-02-02 13:12:11 +0100

    modify CMakeLists and add README

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	sf::RenderWindow renderWindow(sf::VideoMode(100, 100), "Hello LightYears");
+	std::cout << "Hello Worlds!" << std::endl;
+
+	while (renderWindow.isOpen())
+	{
+		sf::Event windowEvent;
+		while (renderWindow.pollEvent(windowEvent))
+		{
+			if(windowEvent.type == sf::Event::EventType::Closed)
+				renderWindow.close();
+		}
+		//renderWindow.clear();
+		//renderWindow.display();
+	}
+
+
+
+
+
 	return 0;
 }
